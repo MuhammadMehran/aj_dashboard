2655ad4f2b48b6a5cabb5b87c1dbf2c843741111
MuhammadMehran <mehranbutt97@gmail.com>
2022-09-23 10:36:51 +0500

    cache fix

--- app.py
+++ app.py
@@ -28,11 +28,14 @@ with row0_2:
 ################
 
 @st.cache(ttl=24*60*60)
-def chart1(band, year, reporting_company):
+def chart1_data(band, year, reporting_company):
     df_filtered = df[df['Reference Year / Année de référence'] == year]
     df_filtered = df_filtered[df_filtered['Band Number'] == band]
     df_filtered = df_filtered[df_filtered['Reporting Company Trade Name / Nom commercial de la société déclarante'] == reporting_company]
+    return df_filtered
 
+
+def chart1(df_filtered):
     total_df_filtered = df_filtered.groupby('Facility Name')[
         'Total Emissions (tonnes CO2e) / Émissions totales (tonnes éq. CO2)'].sum()
     total_df_filtered = total_df_filtered.reset_index()
@@ -45,11 +48,14 @@ def chart1(band, year, reporting_company):
 
 
 @st.cache(ttl=24*60*60)
-def chart2(band, year, naicas_code):
+def chart2_data(band, year, naicas_code):
     df_filtered = df[df['Reference Year / Année de référence'] == year]
     df_filtered = df_filtered[df_filtered['Band Number'] == band]
     df_filtered = df_filtered[df_filtered["English Facility NAICS Code Description / Description du code SCIAN de l'installation en anglais"] == naicas_code]
+    return df_filtered
 
+
+def chart2(df_filtered):
     total_df_filtered = df_filtered.groupby('Facility Name')[
         'Total Emissions (tonnes CO2e) / Émissions totales (tonnes éq. CO2)'].sum()
     total_df_filtered = total_df_filtered.reset_index()
@@ -62,8 +68,12 @@ def chart2(band, year, naicas_code):
 
 
 @st.cache(ttl=24*60*60)
-def chart3(band):
+def chart3_data(band):
     df2 = df[df['Band Number'] == band]
+    return df2
+
+
+def chart3(df2):
     fig = px.bar(df2, x='Facility Name', y='Reference Year / Année de référence',
                  color="English Facility NAICS Code Description / Description du code SCIAN de l'installation en anglais")
 
@@ -89,8 +99,8 @@ with row5_1:
             df['Reporting Company Trade Name / Nom commercial de la société déclarante'].unique()), key='reporting_company', index=92)
 
 with row5_2:
-    chart1(band_chart1, year_chart1, reporting_company)
-
+    df_filtered = chart1_data(band_chart1, year_chart1, reporting_company)
+    chart1(df_filtered)
 ### SEASON ###
 row6_spacer1, row6_1, row6_spacer2 = st.columns((.2, 7.1, .2))
 with row6_1:
@@ -106,8 +116,8 @@ with row7_1:
         "Please select NAICS Code", list(
             df["English Facility NAICS Code Description / Description du code SCIAN de l'installation en anglais"].unique()), key='naicas_code', index=26)
 with row7_2:
-    chart2(band_chart2, year_chart2, naicas_code)
-
+    df_filtered = chart2_data(band_chart2, year_chart2, naicas_code)
+    chart2(df_filtered)
 # ### MATCHDAY ###
 row8_spacer1, row8_1, row8_spacer2 = st.columns((.2, 7.1, .2))
 with row8_1:
@@ -118,4 +128,5 @@ with row9_1:
     band_chart3 = st.selectbox("Please select Band Number", list(
         df['Band Number'].unique()), key='band_chart3')
 with row9_2:
-    chart3(band_chart3)
+    df2 = chart3_data(band_chart3)
+    chart3(df2)
